13b16e466036c198d6c75f6f0d1053dca177f99c
ichengpan <377822835@qq.com>
2019-05-14 14:52:58 +0800

    shiro初始化

---
+++ src/main/java/com/iviui/carlife/modules/user/controller/UserInfoController.java
@@ -0,0 +1,34 @@
+package com.iviui.carlife.modules.user.controller;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author: ChengPan
+ * @date: 2019/5/13
+ * @description: 菜单信息
+ */
+@Controller
+@RequestMapping("/userInfo")
+public class UserInfoController {
+
+    @RequestMapping(value="/userAdd",method= RequestMethod.GET)
+    @RequiresPermissions("userInfo:add")
+    public String userAdd(){
+        return"userAdd";
+    }
+
+    @RequestMapping(value="/userDel",method= RequestMethod.GET)
+    @RequiresPermissions("userInfo:del")
+    public String userDel(){
+        return"userDel";
+    }
+
+    @RequestMapping(value="/userInfo",method= RequestMethod.GET)
+    @RequiresPermissions("userInfo:view")
+    public String userInfo(){
+        return"userInfo";
+    }
+}
---
+++ src/main/java/com/iviui/carlife/modules/user/service/MainService.java
@@ -0,0 +1,9 @@
+package com.iviui.carlife.modules.user.service;
+
+/**
+ * @author: ChengPan
+ * @date: 2019/5/13
+ * @description: 菜单信息
+ */
+public interface MainService {
+}
